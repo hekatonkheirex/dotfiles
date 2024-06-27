1b14c7bab43bf9279c5d3cbf2a98a90b6e385133
Rodrigo Y. Murayama <hekatonkheirex@gmail.com>
2024-06-26 22:49:43 -0400

    Added the rest of the dotfiles

---
+++ nvim/.config/nvim/lua/lualine-config.lua
@@ -0,0 +1,24 @@
+require('lualine').setup({
+  options = { 
+    -- theme = 'rose-pine',
+    -- theme = 'gruvbox-material',
+    -- theme = 'tokyonight',
+    -- theme = 'onedark',
+    theme = 'catppuccin',
+    -- theme = 'nord',
+    -- theme = 'horizon',
+    -- theme = 'everforest',
+    -- theme = 'decay',
+    -- theme = 'ayu',
+    -- theme = 'dracula',
+    -- theme = 'oxocarbon',
+    -- theme = 'everblush',
+    -- theme = '2077',
+    -- theme = 'kanagawa',
+    -- section_separators = '',
+    -- component_separators = '',
+    -- component_separators = { left = '', right = ''},
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''}
+  }
+})
